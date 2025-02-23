feacc58fcb2de490bcd2fc7ec902d6ae5ce0b540
qa <qa@offices-MacBook-Pro.local>
2025-02-23 13:54:03 +0200

    first commit

---
+++ playwright.config.js
@@ -0,0 +1,25 @@
+// @ts-check
+require('dotenv').config();
+const { defineConfig, devices } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './',
+  timeout: 60000,
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 1,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    baseURL: process.env.BASE_URL || 'http://localhost:3000',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure'
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+}); 
