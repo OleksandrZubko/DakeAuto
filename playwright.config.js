876b4760a8b4b03742c36430698616c04d29f734
qa <qa@offices-MacBook-Pro.local>
2025-02-23 15:05:46 +0200

    Fix

--- playwright.config.js
+++ playwright.config.js
@@ -3,13 +3,16 @@ require('dotenv').config();
 const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
-  testDir: './',
+  testDir: './tests',
   timeout: 60000,
-  fullyParallel: true,
+  fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 1,
   workers: 1,
-  reporter: 'html',
+  reporter: [
+    ['list'],
+    ['html']
+  ],
   use: {
     baseURL: process.env.BASE_URL,
     trace: 'on-first-retry',
